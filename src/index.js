f768c4a6a2493db74ae2c9082f92e65cdb22eb2f
anyhhope <hopeanysimova@yandex.ru>
2023-02-08 19:25:35 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let data = [];
+  n = n.toString();
+  while(n.length != 0){
+    data.push(n[n.length - 1]);
+    n = n.substr(0, n.length - 1);
+  }
+  let ansStr = "";
+  data.map(item => ansStr += item)
+  return parseInt(ansStr);
 }
